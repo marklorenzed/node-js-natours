f9c52543016afc7d4398e75c6436a24617fc5640
Mark Lorenze <marklorenzed@gmail.com>
2020-03-19 19:59:16 +0800

    finished global error handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -60,7 +60,6 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
     .limitFields()
     .paginate();
   const tours = await features.query;
-
   // SEND RESPONSE
   res.status(200).json({
     status: 'success',
