fb26125383ea1585966dc503bdce27eacd802abe
Mark Lorenze <marklorenzed@gmail.com>
2020-03-22 19:30:28 +0800

    added password reset functionality

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require("bcryptjs");
@@ -16,6 +17,11 @@ const userSchema = new mongoose.Schema({
         validate: [validator.isEmail, 'Please provide a valid email']
     },
     photo: String,
+    role: {
+        type: String,
+        enum: ['user', 'guide', 'lead-guide', 'admin'],
+        default: 'user'
+    },
     password: {
         type: String,
         required: [true, 'Please provide a password'],
@@ -33,7 +39,14 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords are not the same.' 
         }
     },
-    passwordChangeAt: Date
+    passwordChangeAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 })
 
 userSchema.pre('save', async function(next) {
@@ -48,6 +61,22 @@ userSchema.pre('save', async function(next) {
     next();
 });
 
+userSchema.pre('save', function(next) {
+    if(!this.isModified('password') || this.isNew) return next();
+
+    // putting passwordChangedAt to 1 second in the past will make sure the token is created after the password has been changed.
+    this.passwordChangeAt = Date.now() - 1000;
+    next();
+})
+
+// QUERY MIDDLEWARE
+userSchema.pre(/^find/, function(next) {
+    // this points to the current query
+    this.find({ active: { $ne: false } });
+    next();
+});
+
+// instance methods
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
 }
@@ -61,6 +90,15 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 
     // False means not changed
     return false;
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
 }
 
 const User = mongoose.model('User', userSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,16 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword )
+
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+
 
 router
   .route('/')
