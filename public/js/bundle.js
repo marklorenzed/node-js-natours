a14879e0c91ca5fc66d000b8d7dc029b2ceef3b6
Mark Lorenze <marklorenzed@gmail.com>
2020-04-07 23:54:27 +0800

    added update user photo and tour imageCover and images

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9129,15 +9129,18 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', _login.logout);
-if (userDataForm) userDataForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
-});
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    (0, _updateSettings.updateSettings)(form, 'data');
+  });
+}
+
 if (userPasswordForm) userPasswordForm.addEventListener('submit', /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(e) {
     var passwordCurrent, password, passwordConfirm;
@@ -9203,7 +9206,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62612" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55259" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- public/js/index.js
+++ public/js/index.js
@@ -27,14 +27,17 @@ if (loginForm) {
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm)
+if (userDataForm) {
   userDataForm.addEventListener('submit', e => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
 
-    updateSettings({ name, email }, 'data');
+    updateSettings(form, 'data');
   });
+}
 
 if (userPasswordForm)
   userPasswordForm.addEventListener('submit', async e => {
