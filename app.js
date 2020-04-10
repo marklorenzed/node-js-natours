28e484f71cabb3020aeab4ebda26846117966176
Mark Lorenze Dumalaon <marklorenzed@gmail.com>
2020-04-10 17:32:29 +0800

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,14 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// set Access-Control-Allow-Origin *
+
+// for special methods like put patch delete
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors()); <-- specific
+
 //to enable express to serve static files like images and html
 app.use(express.static(path.join(__dirname, 'public')));
 
